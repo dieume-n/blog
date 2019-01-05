df5f2bb97f2d1dc0ecfbbe0506be7113757f54c8
Dieume <dieume.dev@gmail.com>
2019-01-05 15:12:00 +0300

    creation of Articles controller and routes for CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List All Aticles
+Route::get('articles', 'ArticleController@index');
+
+// List a single article
+Route::get('article/{id}', 'ArticleController@show');
+
+// Create a new article
+Route::post('article', 'ArticleController@store');
+
+// Update article
+Route::put('article', 'ArticleController@store');
+
+// Delete article
+Route::delete('article', 'ArticleController@destroy');
